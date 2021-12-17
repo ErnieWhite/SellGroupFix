49313aaa5996a0e2a7d7d977d76b9441c0a2458b
ErnieWhite <erniewhite@embarqmail.com>
2021-12-17 06:39:01 -0600

    now creates the parts_dict

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from openpyxl import Workbook
+import csv
 
 
 def main():
-    pass
+    parts_dict = {}
+    with open('productpricelinesellgroup.csv', newline='', encoding='latin-1') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            parts_dict[row[1]] = set(row[3:])
+        for k, v in parts_dict.items():
+            print(k, v)
+  #  with open('SalesHistory.csv', newline='', encoding='latin-1') as csv_file:
+
 
 
 if __name__ == '__main__':
